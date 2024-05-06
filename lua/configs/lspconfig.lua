ba7fae07d4b006230d029c4df4ef09e780f48d25
root <rimbojnr@gmail.com>
2024-05-06 12:55:39 +0100

    My config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -20,4 +20,11 @@ lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+}
+
+-- rust
+lspconfig.rust_analyzer.setup {
+  settings = {
+    ['rust-analyzer'] = {},
+  },
 }
